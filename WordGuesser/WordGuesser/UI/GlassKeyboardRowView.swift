dab379f2906265d7865ba6cedf78beae69514921
Juan Diosdado <juan.diosdado@gmail.com>
2026-02-27 00:05:28 +0100

    Add glass keyboard

---
+++ WordGuesser/WordGuesser/UI/GlassKeyboardRowView.swift
@@ -0,0 +1,18 @@
+//
+//  GlassKeyboardRowView.swift
+//  WordGuesser
+//
+//  Created by Juan Diosdado on 2026-02-26.
+//
+
+import SwiftUI
+
+struct GlassKeyboardRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlassKeyboardRowView()
+}
---
+++ WordGuesser/WordGuesser/UI/GlassKeyboardView.swift
@@ -0,0 +1,18 @@
+//
+//  GlassKeyboardView.swift
+//  WordGuesser
+//
+//  Created by Juan Diosdado on 2026-02-26.
+//
+
+import SwiftUI
+
+struct GlassKeyboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlassKeyboardView()
+}
